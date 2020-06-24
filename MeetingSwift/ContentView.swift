8f7e79b7f73b4e9fffe91d5d096cc3b8b8e3de33
iTh1nk <fnchao@hotmail.com>
2020-06-23 19:29:02 -0700

    1st commit

---
+++ MeetingSwift/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MeetingSwift
+//
+//  Created by Chao Feng on 6/23/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetingSwift/MeetingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingSwiftApp.swift
+//  MeetingSwift
+//
+//  Created by Chao Feng on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct MeetingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
