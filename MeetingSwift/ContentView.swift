878e9c1fb37bc90c07f93f89aa6af127508d2683
iTh1nk <fnchao@hotmail.com>
2020-06-24 21:30:10 -0700

    run on device with test data

--- MeetingSwift/ContentView.swift
+++ MeetingSwift/ContentView.swift
@@ -41,5 +41,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(meetings: testData)
+            .preferredColorScheme(.dark)
     }
 }
--- MeetingSwift/MeetingSwiftApp.swift
+++ MeetingSwift/MeetingSwiftApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MeetingSwiftApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(meetings: testData)
         }
     }
 }
