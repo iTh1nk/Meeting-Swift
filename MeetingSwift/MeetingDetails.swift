166e01512a022af1f93d828d654cce08e061d2f6
iTh1nk <fnchao@hotmail.com>
2020-06-24 22:36:09 -0700

    Scrollview; loop more data

--- MeetingSwift/MeetingDetails.swift
+++ MeetingSwift/MeetingDetails.swift
@@ -12,58 +12,73 @@ struct MeetingDetails: View {
     var meetings: Meeting
     
     var body: some View {
-        VStack {
-            HStack {
-                Image("\(meetings.avatar)")
-                    .resizable()
-                    .frame(width: 50, height: 50)
-            }
-            .padding(.all, 30)
-            HStack {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Start At: ")
-                            .font(.headline)
-                        Text("\(meetings.timeStart)")
-                    }
-                    .padding(.all, 5)
-                    HStack {
-                        Text("End At: ")
-                            .font(.headline)
-                        Text("\(meetings.timeEnd)")
-                    }
-                    .padding(.all, 5)
-                    HStack {
-                        Text("Location: ")
-                            .font(.headline)
-                        Text("\(meetings.location)")
-                    }
-                    .padding(.all, 5)
-                    HStack {
-                        Text("Hosts: ")
-                            .font(.headline)
-                        Text("\(meetings.hosts)")
-                    }
-                    .padding(.all, 5)
-                    HStack {
-                        Text("Attendees: ")
-                            .font(.headline)
-                        Text("\(meetings.attendees)")
-                    }
-                    .padding(.all, 5)
+        ScrollView {
+            VStack {
+                HStack {
+                    Image("\(meetings.avatar)")
+                        .resizable()
+                        .frame(width: 50, height: 50)
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.all, 30)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Start At: ")
+                                .font(.system(size: 20))
+                                .foregroundColor(.green)
+                            Text("\(meetings.timeStart)")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.all, 5)
+                        HStack {
+                            Text("End At: ")
+                                .font(.system(size: 20))
+                                .foregroundColor(.green)
+                            Text("\(meetings.timeEnd)")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.all, 5)
+                        HStack {
+                            Text("Location: ")
+                                .font(.system(size: 20))
+                                .foregroundColor(.green)
+                            Text("\(meetings.location)")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.all, 5)
+                        HStack {
+                            Text("Hosts: ")
+                                .font(.system(size: 20))
+                                .foregroundColor(.green)
+                            Text("\(meetings.hosts)")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.all, 5)
+                        HStack {
+                            Text("Attendees: ")
+                                .font(.system(size: 20))
+                                .foregroundColor(.green)
+                            Text("\(meetings.attendees)")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.all, 5)
+                    }
+                    Spacer()
+                }
+                .padding(.leading, 20)
+                .navigationTitle(Text(meetings.title))
                 Spacer()
             }
-            .padding(.leading, 20)
-            .navigationTitle(Text(meetings.title))
-            Spacer()
+            .frame(maxWidth: .infinity)
         }
-        .frame(maxWidth: .infinity)
     }
 }
 
 struct MeetingDetails_Previews: PreviewProvider {
     static var previews: some View {
         MeetingDetails(meetings: testData[0])
+            .preferredColorScheme(.dark)
     }
 }
